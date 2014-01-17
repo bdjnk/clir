3fa51349636957a55b4d1a12b1eaaf6981b42830
bdjnk <bdjnks@gmail.com>
2014-01-17 15:21:28 -0800

    Make use of the new completion / name

--- example.c
+++ example.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "linenoise.h"
+#include "clir.h"
 
 
-void completion(const char *buf, linenoiseCompletions *lc) {
+void completion(const char *buf, clirCompletions *lc) {
     if (buf[0] == 'h') {
-        linenoiseAddCompletion(lc,"hello");
-        linenoiseAddCompletion(lc,"hello there");
+        clirAddCompletion(lc,"hello");
+        clirAddCompletion(lc,"hi");
+        clirAddCompletion(lc,"hey");
+        clirAddCompletion(lc,"howzit");
     }
 }
 
@@ -20,7 +22,7 @@ int main(int argc, char **argv) {
         argc--;
         argv++;
         if (!strcmp(*argv,"--multiline")) {
-            linenoiseSetMultiLine(1);
+            clirSetMultiLine(1);
             printf("Multi-line mode enabled.\n");
         } else {
             fprintf(stderr, "Usage: %s [--multiline]\n", prgname);
@@ -30,28 +32,28 @@ int main(int argc, char **argv) {
 
     /* Set the completion callback. This will be called every time the
      * user uses the <tab> key. */
-    linenoiseSetCompletionCallback(completion);
+    clirSetCompletionCallback(completion);
 
     /* Load history from file. The history file is just a plain text file
      * where entries are separated by newlines. */
-    linenoiseHistoryLoad("history.txt"); /* Load the history at startup */
+    clirHistoryLoad("history.txt"); /* Load the history at startup */
 
-    /* Now this is the main loop of the typical linenoise-based application.
-     * The call to linenoise() will block as long as the user types something
+    /* Now this is the main loop of the typical clir-based application.
+     * The call to clir() will block as long as the user types something
      * and presses enter.
      *
      * The typed string is returned as a malloc() allocated string by
-     * linenoise, so the user needs to free() it. */
-    while((line = linenoise("hello> ")) != NULL) {
+     * clir, so the user needs to free() it. */
+    while((line = clir("hello> ")) != NULL) {
         /* Do something with the string. */
         if (line[0] != '\0' && line[0] != '/') {
-            printf("echo: '%s'\n", line);
-            linenoiseHistoryAdd(line); /* Add to the history. */
-            linenoiseHistorySave("history.txt"); /* Save the history on disk. */
+            //printf("echo: '%s'\n", line);
+            clirHistoryAdd(line); /* Add to the history. */
+            clirHistorySave("history.txt"); /* Save the history on disk. */
         } else if (!strncmp(line,"/historylen",11)) {
             /* The "/historylen" command will change the history len. */
             int len = atoi(line+11);
-            linenoiseHistorySetMaxLen(len);
+            clirHistorySetMaxLen(len);
         } else if (line[0] == '/') {
             printf("Unreconized command: %s\n", line);
         }
